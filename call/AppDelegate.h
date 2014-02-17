f12d4a475263ac50ed816002846a4633769182fa
Lokesh <macuser@Macs-iMac.local>
2014-02-17 19:19:23 +0530

    Initial Commit

---
+++ call/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  call
+//
+//  Created by Lokesh on 2/17/14.
+//  Copyright (c) 2014 Mac User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ call/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  call
+//
+//  Created by Lokesh on 2/17/14.
+//  Copyright (c) 2014 Mac User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
